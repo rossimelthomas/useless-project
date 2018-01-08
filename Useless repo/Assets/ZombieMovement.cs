3d9b6b8d02f6228fbcfe1426c335b9eb8d9d42d2
Thomas rossi mel <rossimelthomas@gmail.com>
2018-01-08 12:15:08 +0100

    test

---
+++ Useless repo/Assets/ZombieMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour {
+
+    const int movementSpeed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position += Vector3.forward * Time.deltaTime * movementSpeed;
+        
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            other.SendMessage("die");
+        }
+        else
+        { 
+            transform.Rotate(new Vector3(0, Random.Range(0, 1) >= 0.5 ? -50 : 50, 0) * Time.deltaTime);
+        } 
+    }
+
+
+}
