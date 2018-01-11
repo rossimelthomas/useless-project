1c9285f23f710b346a402b032e7729604e543bc6
Giovanni Coronica <jacket.gav@gmail.com>
2018-01-11 12:25:55 +0100

    cose

---
+++ Useless repo/Assets/FlashLight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashLight : MonoBehaviour
+{
+    private Light myLight;
+
+
+    void Start()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
+    }
+}
