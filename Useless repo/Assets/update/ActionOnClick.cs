bda5ed99821389105f76644b3ed5e7aaee39dbfd
Giovanni Coronica <jacket.gav@gmail.com>
2018-01-12 08:35:59 +0100

    menu

---
+++ Useless repo/Assets/update/ActionOnClick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActionOnClick : MonoBehaviour {
+    
+
+
+    public void OnMouseUp()
+    {
+        if (this.name == "Start Game")
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (this.name == "Credits")
+        {
+            //SceneManager.LoadScene("MazeGenerator\\demo");
+        }
+        if (this.name == "Quit")
+        {
+            Application.Quit();
+        }
+
+    }
+}
---
+++ Useless repo/Assets/update/MouseHover.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour {
+
+    /*void Start() { }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000f))
+        {
+            Debug.Log("top");
+            GameObject.Find("Start Game").GetComponent<Renderer>().material.color = Color.green;
+        }
+    }*/
+    
+    bool mouseOver = false;
+
+    void OnMouseEnter()
+    {
+        mouseOver = true;
+        if (this.name == "Start Game")
+        {
+            GetComponent<Transform>().localScale += new Vector3(0.1F, 0.1F, 0);
+            GetComponent<Transform>().localPosition = new Vector3(2.9F, 9.84F, 0.19F);
+        }
+        if (this.name == "Credits")
+        {
+            GetComponent<Transform>().localScale += new Vector3(0.1F, 0.1F, 0);
+            GetComponent<Transform>().localPosition = new Vector3(-1.11F, 7.56F, 0.41F);
+        }
+        if (this.name == "Quit")
+        {
+            GetComponent<Transform>().localScale += new Vector3(0.1F, 0.1F, 0);
+            GetComponent<Transform>().localPosition = new Vector3(-4.03F, 5.3F, 0.58F);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        mouseOver = false;
+        if (this.name == "Start Game")
+        {
+            GetComponent<Transform>().localScale -= new Vector3(0.1F, 0.1F, 0);
+            GetComponent<Transform>().localPosition = new Vector3(0.11F, 9.84F, 0.26F);
+        }
+        if (this.name == "Credits")
+        {
+            GetComponent<Transform>().localScale -= new Vector3(0.1F, 0.1F, 0);
+            GetComponent<Transform>().localPosition = new Vector3(-2.6F, 7.56F, 0.44F);
+        }
+        if (this.name == "Quit")
+        {
+            GetComponent<Transform>().localScale -= new Vector3(0.1F, 0.1F, 0);
+            GetComponent<Transform>().localPosition = new Vector3(-4.8F, 5.3F, 0.6F);
+        }
+    }
+}
