bc13995fb3b7f7e0dfa84931b292de5dc9080d0a
Giovanni Coronica <jacket.gav@gmail.com>
2018-01-12 11:53:08 +0100

    robe

---
+++ Useless repo/Assets/OpenTreasure.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTreasure : MonoBehaviour {
+
+    void Update()
+    {
+
+    }
+    
+    void Start()
+    {
+
+    }
+
+	void OnTriggerEnter(Collider obj)
+    {
+        Debug.Log(obj.gameObject.name + "ciao");
+        Debug.Log(  "ciao");
+        if (obj.gameObject.name == "FPSController")
+        {
+            GetComponent<Animator>().SetBool("open", true);
+        }
+    }
+}
